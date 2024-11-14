b9d44b0847d150b85a96e435f561ac371ca7d3dc
clineb12 <123430834+clineb12@users.noreply.github.com>
2024-11-14 13:20:36 -0500

    wip

--- index5.php
+++ index5.php
@@ -11,8 +11,8 @@ if (!is_logged_in()) {
 
 $host = 'localhost'; 
 $dbname = 'books'; 
-$user = 'mark'; 
-$pass = 'mark';
+$user = 'brooke'; 
+$pass = 'passwd';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
